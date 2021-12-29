2f024755ae61b9ebdcefb3dd6039fac236c255f9
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-12-29 11:29:41 -0300

    Inheritancd

---
+++ src/chapter9/Inheritance.java
@@ -0,0 +1,15 @@
+package chapter9;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        PersonInheritance person = new PersonInheritance();
+        EmployeeInheritance employee = new EmployeeInheritance();
+
+
+    }
+
+
+
+}
